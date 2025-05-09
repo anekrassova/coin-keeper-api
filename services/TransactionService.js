b0d198acf3cb1114f7c08a7849b9240f4f63db05
anekrassova <nni44561@gmail.com>
2025-05-09 22:49:13 +0500

    Доработка логики для транзакций

--- services/TransactionService.js
+++ services/TransactionService.js
@@ -1,4 +1,6 @@
 import { Transaction } from '../models/transaction.js';
+import { IncomeCategory } from '../models/incomeCategory.js';
+import { ExpenseCategory } from '../models/expenseCategory.js';
 import { Account } from '../models/account.js';
 import { User } from '../models/user.js';
 import { convertCurrency, currencyInSign } from '../libs/convertCurrency.js';
@@ -21,32 +23,51 @@ export class TransactionService {
       );
       const currency = user.preffered_currency;
 
+      let fromName, toName;
+
+      // Определяем fromName и toName в зависимости от типа транзакции
       switch (type) {
          case 'income':
+            const incomeCategory = await IncomeCategory.findById(from);
+            fromName = incomeCategory
+               ? incomeCategory.title
+               : 'Unknown Income Category';
             const incomeAccount = await Account.findByIdAndUpdate(
                to,
                { $inc: { amount: amountInKZT } },
                { new: true }
             );
+            toName = incomeAccount ? incomeAccount.title : 'Unknown Account';
             break;
+
          case 'expense':
             const expenseAccount = await Account.findByIdAndUpdate(
                from,
                { $inc: { amount: -amountInKZT } },
                { new: true }
             );
+            fromName = expenseAccount
+               ? expenseAccount.title
+               : 'Unknown Account';
+            const expenseCategory = await ExpenseCategory.findById(to);
+            toName = expenseCategory
+               ? expenseCategory.title
+               : 'Unknown Expense Category';
             break;
+
          case 'transfer':
             const fromAccount = await Account.findByIdAndUpdate(
                from,
                { $inc: { amount: -amountInKZT } },
                { new: true }
             );
+            fromName = fromAccount ? fromAccount.title : 'Unknown Account';
             const toAccount = await Account.findByIdAndUpdate(
                to,
                { $inc: { amount: amountInKZT } },
                { new: true }
             );
+            toName = toAccount ? toAccount.title : 'Unknown Account';
             break;
       }
 
@@ -81,6 +102,8 @@ export class TransactionService {
          type: transaction.type,
          from: transaction.from,
          to: transaction.to,
+         fromName, // добавляем fromName
+         toName, // добавляем toName
          date: transaction.date,
          amount: String(amount) + currencyInSign[currency],
          comment: transaction.comment,
@@ -111,8 +134,14 @@ export class TransactionService {
       const prevTransactionAmount = transactionToUpdate.amount;
       const newTransactionAmount = amountInKZT;
 
+      let fromName, toName;
+
       switch (transactionToUpdate.type) {
          case 'income':
+            const incomeCategory = await IncomeCategory.findById(from);
+            fromName = incomeCategory
+               ? incomeCategory.title
+               : 'Unknown Income Category';
             const incomeAccount = await Account.findByIdAndUpdate(
                to,
                {
@@ -122,7 +151,9 @@ export class TransactionService {
                },
                { new: true }
             );
+            toName = incomeAccount ? incomeAccount.title : 'Unknown Account';
             break;
+
          case 'expense':
             const expenseAccount = await Account.findByIdAndUpdate(
                from,
@@ -133,7 +164,15 @@ export class TransactionService {
                },
                { new: true }
             );
+            fromName = expenseAccount
+               ? expenseAccount.title
+               : 'Unknown Account';
+            const expenseCategory = await ExpenseCategory.findById(to);
+            toName = expenseCategory
+               ? expenseCategory.title
+               : 'Unknown Expense Category';
             break;
+
          case 'transfer':
             const fromAccount = await Account.findByIdAndUpdate(
                from,
@@ -144,6 +183,7 @@ export class TransactionService {
                },
                { new: true }
             );
+            fromName = fromAccount ? fromAccount.title : 'Unknown Account';
             const toAccount = await Account.findByIdAndUpdate(
                to,
                {
@@ -153,6 +193,7 @@ export class TransactionService {
                },
                { new: true }
             );
+            toName = toAccount ? toAccount.title : 'Unknown Account';
             break;
       }
 
@@ -169,6 +210,8 @@ export class TransactionService {
          type: updatedTransaction.type,
          from: updatedTransaction.from,
          to: updatedTransaction.to,
+         fromName, // добавляем fromName
+         toName, // добавляем toName
          date: updatedTransaction.date,
          amount: String(amount) + currencyInSign[updatedTransaction.currency],
          comment: updatedTransaction.comment,
@@ -196,18 +239,43 @@ export class TransactionService {
          date: { $gte: startOfMonth, $lte: endOfMonth },
       });
 
-      const formattedTransactions = transactions.map((t) => {
-         const obj = t.toObject();
-         const { _id, ...rest } = obj;
+      const formattedTransactions = await Promise.all(
+         transactions.map(async (t) => {
+            const obj = t.toObject();
+            const { _id, from, to, type, ...rest } = obj;
 
-         return {
-            id: _id,
-            ...rest,
-            amount: String(t.amount) + currencyInSign[t.currency],
-            current_balance:
-               String(t.current_balance) + currencyInSign[t.currency],
-         };
-      });
+            let fromName = '';
+            let toName = '';
+
+            // Поиск fromName и toName в зависимости от типа транзакции
+            if (type === 'income') {
+               const incomeCategory = await IncomeCategory.findById(from);
+               const account = await Account.findById(to);
+               fromName = incomeCategory ? incomeCategory.title : '';
+               toName = account ? account.title : '';
+            } else if (type === 'expense') {
+               const account = await Account.findById(from);
+               const expenseCategory = await ExpenseCategory.findById(to);
+               fromName = account ? account.title : '';
+               toName = expenseCategory ? expenseCategory.title : '';
+            } else if (type === 'transfer') {
+               const fromAccount = await Account.findById(from);
+               const toAccount = await Account.findById(to);
+               fromName = fromAccount ? fromAccount.title : '';
+               toName = toAccount ? toAccount.title : '';
+            }
+
+            return {
+               id: _id,
+               fromName,
+               toName,
+               ...rest,
+               amount: String(t.amount) + currencyInSign[t.currency],
+               current_balance:
+                  String(t.current_balance) + currencyInSign[t.currency],
+            };
+         })
+      );
 
       return {
          status: 200,
